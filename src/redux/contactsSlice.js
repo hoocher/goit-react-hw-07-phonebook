5da0d41daa6c2a8f92b6308eb1b318f5d649de2a
Dmytro Huriev <hooch.inbox@gmail.com>
2023-10-17 23:40:51 +0300

    ready to exam 001

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -43,8 +43,9 @@ const contactsSlice = createSlice({
       state.error = null;
 
       const index = state.items.findIndex(
-        contact => contact.id === action.payload
+        contact => contact.id === action.payload.id
       );
+      console.log('index', index);
       state.items.splice(index, 1);
     },
   },
